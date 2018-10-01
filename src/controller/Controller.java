b7375c85d956a8584e729d5513f052b6ce55de37
mitooos <ma.hoyosr@uniandes.edu.co>
2018-10-01 12:58:28 -0500

    definicion de los requerimientos dentro de el view

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,54 @@
 package controller;
 
+
+
 public class Controller {
 	
-	public static String metodo1() {
-		return "Holoo";
+	public static void registrarProveedor() {
+		
 	}
+	
+	public static void registrarProdcuto() {
+		
+	}
+	
+	public static void registrarCliente() {
+		
+	}
+	
+	public static void registrarSucursal() {
+		
+	}
+	
+	public static void registrarUnaBodegaAUnaSucursal() {
+		
+	}
+	
+	public static void registrarUnEstanteAUnaSucursal() {
+		
+	}
+	
+	public static void registrarUnaPromocion() {
+		
+	}
+	
+	public static void finalizarUnaPromocion() {
+		
+	}
+	
+	public static void registrarUnPediddoDeUnProductoAUnaSucursal() {
+		
+	}
+	
+	public static void registrarLaLlegadaDeUnProductoAUnaSucursal() {
+		
+	}
+	
+	public static void registrarLaVentaDeUnProductoEnUnaSucursal() {
+		
+	}
+	
+	// faltan metodos de consulta
+	
 
 }
