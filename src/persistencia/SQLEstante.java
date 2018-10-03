6984fe60e4e825c9ff444020d9b4efc9b095bf5d
sebastianvaca99 <js.vacat@uniandes.edu.co>
2018-10-03 04:12:36 -0500

    Varias implementaciones

--- src/persistencia/SQLEstante.java
+++ src/persistencia/SQLEstante.java
@@ -18,4 +18,23 @@ private final static String SQL = SuperAndesPersistence.SQL;
 		q.setParameters(id, capacidadVol, capacidadPeso, categoria,posicion,nivelAbastecimiento, idSucursal);
 		return (long) q.executeUnique();	
 	}
+	
+	
+	public  Integer  darIndiceOcupacion(PersistenceManager pm,Long idSucursal, Long idEstante)
+	{	
+		String sql2 ="SELECT SUM(PRODUCTO.PESO_EMPAQUE) AS PESOACTUAL ,ESTANTE_PRODUCTO.ID_BODEGA AS ID";
+		sql2 += " FROM "+ sap.darTablaEstanteProducto();
+		sql2 += " JOIN " + sap.darTablaProductos() + "ON ID = ID_PRODUCTO";
+		sql2 += " GROUP BY ID_ESTANTE";
+		
+		
+		String sql1 ="SELECT F.PESOACTUAL/ESTANTE.CAPACIDAD_PESO AS INDICE, ESTANTE.ID AS ID";
+		sql1 += " FROM (" + sql2 + ")f";
+		sql1 += " JOIN " + sap.darTablaEstante();
+		sql1 += " ON ID = ID";
+		sql1 += " WHERE ID_SUCURSAL = ? AND ID = ?";
+		Query q = pm.newQuery(SQL, sql1);
+		q.setParameters(idSucursal,idEstante);
+		return (Integer) q.executeUnique();
+	}
 }
